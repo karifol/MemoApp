7fa8f23346dd41868048792a28ebf1dbd41bc43f
堀ノ内海斗 <karifol@mac-book.local>
2024-01-14 11:42:38 +0900

    update folder

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>こんにちわ</Text>
+      <Text>おはよう</Text>
+      <Text>びろーん</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color:"#ffffff",
   },
 });
