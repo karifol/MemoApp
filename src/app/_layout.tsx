73dd219648665b10739d6bc9e09ee240aa7a5043
karifol <karigolhorinouchi@gmail.com>
2024-01-18 09:41:50 +0900

    dev

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,4 +5,3 @@ const Layout = (): JSX.Element => {
 }
 
 export default Layout
-
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2021年1月1日 00:00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2021年1月1日 00:00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2021年1月1日 00:00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default index
