a4e6a3e7760e6e2e0e036addb3ee7616083c469d
karifol <karigolhorinouchi@gmail.com>
2024-01-23 20:05:31 +0900

    dev

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,106 +1,104 @@
-import { 
-    View, Text, TextInput, 
-    StyleSheet, TouchableOpacity, Alert
+import {
+  View, Text, TextInput,
+  StyleSheet, TouchableOpacity, Alert
 } from 'react-native'
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
-
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../config'
 import Button from '../../components/Button'
 
 const handlePress = (email: string, password: string): void => {
-    console.log(email, password)
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredintial) => {
-            console.log(userCredintial.user.uid)
-            router.replace('/memo/list')
-        })
-        .catch((error) => {
-            const { code, message} = error
-            console.log(code, message)
-            Alert.alert(message)
-        })
-    
+  console.log(email, password)
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredintial) => {
+      console.log(userCredintial.user.uid)
+      router.replace('/memo/list')
+    })
+    .catch((error) => {
+      const { code, message } = error
+      console.log(code, message)
+      Alert.alert(String(message))
+    })
 }
 
-const SignUp = (): JSX.Element =>{
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    return(
-        <View style = {styles.container}>
-            <View style = {styles.inner}>
-                <Text style = {styles.title}>Sign Up</Text>
-                <TextInput 
-                    style = {styles.input} 
-                    value = {email}
-                    onChangeText = {(text) => {setEmail(text)}}
-                    autoCapitalize = 'none'
-                    keyboardType = 'email-address'
-                    placeholder = 'Email address'
-                    textContentType = 'emailAddress'
-                />
-                <TextInput 
-                    style = {styles.input} 
-                    value = {password}
-                    onChangeText = {(text) => {setPassword(text)}}
-                    autoCapitalize = 'none'
-                    secureTextEntry
-                    placeholder = 'Password'
-                    textContentType = 'password'
-                />
-                <Button label='Submit' onPress={() => {handlePress(email, password)}}/>
-                <View style = {styles.footer}>
-                    <Text style = {styles.footerText}>Already registered?</Text>
-                    <Link href='auth/log_in' asChild replace>
-                        <TouchableOpacity>
-                            <Text style = {styles.footerLink}>Log In</Text>
-                        </TouchableOpacity>
-                    </Link>
-                </View>
-            </View>
+const SignUp = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  return (
+    <View style = {styles.container}>
+      <View style = {styles.inner}>
+        <Text style = {styles.title}>サインアップ</Text>
+        <TextInput
+          style = {styles.input}
+          value = {email}
+          onChangeText = {(text) => { setEmail(text) }}
+          autoCapitalize = 'none'
+          keyboardType = 'email-address'
+          placeholder = 'Email address'
+          textContentType = 'emailAddress'
+        />
+        <TextInput
+          style = {styles.input}
+          value = {password}
+          onChangeText = {(text) => { setPassword(text) }}
+          autoCapitalize = 'none'
+          secureTextEntry
+          placeholder = 'Password'
+          textContentType = 'password'
+        />
+        <Button label='登録' onPress={() => { handlePress(email, password) }}/>
+        <View style = {styles.footer}>
+          <Text style = {styles.footerText}>ログインは</Text>
+          <Link href='auth/log_in' asChild replace>
+            <TouchableOpacity>
+              <Text style = {styles.footerLink}>こちら</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
-    )
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#F0F4F8',
-    },
-    inner:{
-        paddingVertical: 24,
-        paddingHorizontal: 27
-    },
-    title:{
-        fontSize: 24,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        marginBottom: 24
-    },
-    input:{
-        borderWidth: 1,
-        borderColor: '#DDDDDD',
-        backgroundColor: '#ffffff',
-        height: 48,
-        padding: 8,
-        fontSize: 16,
-        marginBottom: 16
-    },
-    footer:{
-        flexDirection: 'row'
-    },
-    footerText:{
-        fontSize: 14,
-        lineHeight: 24,
-        marginRight: 8,
-        color: '#000000'
-    },
-    footerLink:{
-        fontSize: 14,
-        lineHeight: 24,
-        color: '#467FD3'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8'
+  },
+  inner: {
+    paddingVertical: 24,
+    paddingHorizontal: 27
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#DDDDDD',
+    backgroundColor: '#ffffff',
+    height: 48,
+    padding: 8,
+    fontSize: 16,
+    marginBottom: 16
+  },
+  footer: {
+    flexDirection: 'row'
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+    color: '#000000'
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#3A822E'
+  }
 })
 
 export default SignUp
--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,7 +1,6 @@
 import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native'
 import { signOut } from 'firebase/auth'
 import { router } from 'expo-router'
-
 import { auth } from '../config'
 
 const handlePress = (): void => {
@@ -17,16 +16,17 @@ const handlePress = (): void => {
 const LogOutButton = (): JSX.Element => {
   return (
     <TouchableOpacity onPress={handlePress}>
-      <Text style = {styles.text}>Log Out</Text>
+      <Text style = {styles.text}>ログアウト</Text>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 12,
+    fontSize: 15,
     lineHeight: 24,
-    color: 'rgba(255, 255, 255, 0.7)'
+    fontWeight: 'bold',
+    color: 'rgba(255, 255, 255, 0.8)'
   }
 })
 
